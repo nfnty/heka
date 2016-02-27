d04a4824e95712c15a88300f2ae6ec2674eb9125
nfnty <git@nfnty.se>
2016-02-27 08:24:45 +0100

    UlogdDecoder: Implement time_location

--- plugins/nfnty/ulogd_decoder.go
+++ plugins/nfnty/ulogd_decoder.go
@@ -32,29 +32,52 @@ const RFC3339Micro string = "2006-01-02T15:04:05.999999"
 var re = regexp.MustCompile(".*\\.(.*)")
 
 // UlogdDecoder is the backbone of the plugin
-type UlogdDecoder struct{}
+type UlogdDecoder struct {
+	timeLocation *time.Location
+}
 
 // UlogdDecoderConfig contains user configuration
-type UlogdDecoderConfig struct{}
+type UlogdDecoderConfig struct {
+	TimeLocation string `toml:"time_location"`
+}
 
 // ConfigStruct initializes the configuration with defaults
 func (decoder *UlogdDecoder) ConfigStruct() interface{} {
-	return new(UlogdDecoderConfig)
+	return &UlogdDecoderConfig{
+		TimeLocation: time.Local.String(),
+	}
 }
 
 // Init initializes the plugin
 func (decoder *UlogdDecoder) Init(config interface{}) (err error) {
+	conf := config.(*UlogdDecoderConfig)
+	decoder.timeLocation, err = time.LoadLocation(conf.TimeLocation)
 	return
 }
 
-func decodeJSON(key string, value interface{}) (field *message.Field, err error) {
+func (decoder *UlogdDecoder) parseTimestamp(timestamp string) (t int64, err error) {
+	pTime, err := time.ParseInLocation(RFC3339Micro, timestamp, decoder.timeLocation)
+	if err != nil {
+		err = errors.New("Failed to parse timestamp")
+		return
+	}
+	if pTime.IsZero() {
+		err = errors.New("Timestamp is zero")
+		return
+	}
+	return pTime.UnixNano(), err
+}
+
+func (decoder *UlogdDecoder) parseJSON(key string, value interface{}) (field *message.Field, err error) {
 	switch val := value.(type) {
 	case string:
 		if key == "@Timestamp" || key == "timestamp" {
-			if pValue, e := time.ParseInLocation(RFC3339Micro, val, time.Local); e == nil {
-				field, err = message.NewField("@Timestamp", pValue, "")
+			var pValue int64
+			if pValue, err = decoder.parseTimestamp(val); err != nil {
 				return
 			}
+			field, err = message.NewField("@Timestamp", pValue, "")
+			return
 		}
 		field, err = message.NewField(key, val, "")
 
@@ -96,7 +119,7 @@ func (decoder *UlogdDecoder) Decode(pack *pipeline.PipelinePack) (packs []*pipel
 
 	for key, value := range jMessage.(map[string]interface{}) {
 		var field *message.Field
-		if field, err = decodeJSON(key, value); err != nil {
+		if field, err = decoder.parseJSON(key, value); err != nil {
 			return
 		}
 		pack.Message.AddField(field)
